470baf8bf7b14b5657990d4bb1030c48516c26b0
eieneul <eieneul@yahoo.com>
2019-02-12 09:59:37 +1100

    数据结构与算法

--- 16.py
+++ 16.py
@@ -87,17 +87,49 @@ print(prices2)
 
 
 # 嵌套列表
-names = ['关羽', '张飞', '赵云', '马超', '黄忠']
-courses = ['语文', '数学', '英语']
-# 错误 - 参考http://pythontutor.com/visualize.html#mode=edit
-# scores = [[None] * len(courses)] * len(names)
-scores = [[None] * len(courses) for _ in range(len(names))]
-for row, name in enumerate(names):
-    for col, course in enumerate(courses):
-        scores[row][col] = float(input(f'请输入{name}的{course}成绩： '))
-        print(scores)
+def qian_tao_list():
+    names = ['关羽', '张飞', '赵云', '马超', '黄忠']
+    courses = ['语文', '数学', '英语']
+    # 错误 - 参考http://pythontutor.com/visualize.html#mode=edit
+    # scores = [[None] * len(courses)] * len(names)
+    scores = [[None] * len(courses) for _ in range(len(names))]
+    for row, name in enumerate(names):
+        for col, course in enumerate(courses):
+            scores[row][col] = float(input(f'请输入{name}的{course}成绩： '))
+            print(scores)
 
 
 # heapq, itertools等的用法：
 import heapq
+"""
+从列表中找出最大的或最小的N个元素
+"""
+import heapq
 
+list1 = [34, 25, 12, 99, 87, 63, 58, 78, 88, 92]
+list2 = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+print(heapq.nlargest(3, list1))
+print(heapq.nsmallest(3, list1))
+print(heapq.nlargest(2, list2, key=lambda x: x['price']))
+print(heapq.nlargest(2, list2, key=lambda x: x['shares']))
+
+"""
+排列 / 组合 / 笛卡尔积
+"""
+import itertools
+
+for val in itertools.permutations('ABCD'):
+    print(val)
+
+for val in itertools.combinations('ABCDE', 3):
+    print(val)
+
+for val in itertools.product('ABCD', '123'):
+    print(val)
