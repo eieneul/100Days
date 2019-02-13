775ed176b172062022d709b088e5bba6c05b7487
eieneul <eieneul@yahoo.com>
2019-02-13 14:53:41 +1100

    面向对象相关练习

--- 18.py
+++ 18.py
@@ -99,5 +99,113 @@ def main():
         print('%s: %.2f元' % (emp.name, emp.get_salary()))
 
 
+# if __name__ == '__main__':
+#     main()
+
+# 类与类之间的关系：
+# is-a   继承
+# has-a  关联 聚合 合成
+# use-a  依赖
+
+
+# 扑克游戏
+from enum import Enum, unique
+import random
+
+"""
+经验：符号常量总是优于字面常量，枚举类型是定义符号常量的最佳选择
+"""
+
+@unique
+class Suite(Enum):
+    """花色"""
+    SPADE, HEART, CLUB, DIAMOND = range(4)
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+
+class Card(object):
+    """牌"""
+
+    def __init__(self, suite, face):
+        self._suite = suite
+        self._face = face
+
+    def show(self):
+        """显示牌面"""
+        suites = ['♠', '♥', '♣', '♦']
+        faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        return f'{suites[self._suite.value]} {faces[self._face]}'
+
+    def __str__(self):
+        return self.show()
+
+    def __repr__(self):
+        return self.show()
+
+
+class Poker(object):
+    """扑克"""
+    def __init__(self):
+        self.index = 0
+        self.cards = [Card(suite, face)
+                      for suite in Suite
+                      for face in range(0, 13)]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+        self.index = 0
+
+    def deal(self):
+        """发牌"""
+        card = self.cards[self.index]
+        self.index += 1
+        return card
+
+    @property
+    def has_more(self):
+        return self.index < len(self.cards)
+
+
+class Player(object):
+    """玩家"""
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+
+    def get_one(self, card):
+        """摸一张牌"""
+        self.cards.append(card)
+
+    def sort(self, comp=lambda card: (card._suite, card._face)):
+        """整理手牌"""
+        self.cards.sort(key=comp)
+
+
+def main_2():
+    poker = Poker()
+    poker.shuffle()
+    players = [Player('Chen'), Player('Wang'), Player('Li'), Player('Zhang')]
+    while poker.has_more:
+        for player in players:
+            player.get_one(poker.deal())
+    for player in players:
+        player.sort()
+        print(player.name, end=': ')
+        print(player.cards)
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    main_2()
+
+
+
+
+
+
+
+
+
+
